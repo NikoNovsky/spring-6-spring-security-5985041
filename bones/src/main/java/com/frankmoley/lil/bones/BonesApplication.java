f99fd47ff36e8be31ff08ee3c52ece77c4bec2d0
NikoNovsky <nikonovsky@gmail.com>
2026-02-01 00:22:18 +0100

    chenge methid to webflux

--- bones/src/main/java/com/frankmoley/lil/bones/BonesApplication.java
+++ bones/src/main/java/com/frankmoley/lil/bones/BonesApplication.java
@@ -2,12 +2,44 @@ package com.frankmoley.lil.bones;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.reactive.config.EnableWebFlux;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
+@EnableWebFluxSecurity
 public class BonesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BonesApplication.class, args);
 	}
 
+	@Bean
+	public MapReactiveUserDetailsService userDetailsService() {
+		List<UserDetails> userDetails = new ArrayList<>();
+		userDetails.add(User.withDefaultPasswordEncoder()
+				.username("user")
+				.password("password")
+				.roles("USER")
+				.build());
+		return new MapReactiveUserDetailsService(userDetails);
+	}
+
+	@Bean
+	public SecurityWebFilterChain springSecurityWebFilterChain(ServerHttpSecurity http) {
+		http.authorizeExchange(exchanges -> exchanges
+				.anyExchange().authenticated())
+			.httpBasic(Customizer.withDefaults())
+			.formLogin(Customizer.withDefaults());
+		return http.build();
+	}
 }
